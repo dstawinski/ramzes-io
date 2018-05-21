bf2771e62975fad86449c5619d45ee0f4ac11e58
Piotr Rosłaniec <p.roslaniec@gmail.com>
2018-05-21 19:05:38 +0200

    Migration WIP

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+<h1>Empty Landing Page</h1>
+  <router-link to="/dashboard">Dashboard</router-link>
+</div>
+</template> 
+
+<script>
+export default {
+  name: 'LandingPage',
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Dashboard from '@/components/Dashboard';
+import LandingPage from '@/components/LandingPage';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'LandingPage',
+      component: LandingPage,
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
     },
   ],
 });
